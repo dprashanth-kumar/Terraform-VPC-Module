020eca4fb62b986c11df47320cd93c5c41934a91
dprashanth-kumar <dprashanthtinku@gmail.com>
2025-11-22 17:37:01 +0530

    Update variables.tf

--- AWS_First_My_VPC_Project/variables.tf
+++ AWS_First_My_VPC_Project/variables.tf
@@ -3,3 +3,4 @@ variable "project_name" {}
 variable "vpc_cidr" {}
 variable "public_subnets" { type = list(string) }
 variable "private_subnets" { type = list(string) }
+variable "availability_zones" { type = list(string) }
