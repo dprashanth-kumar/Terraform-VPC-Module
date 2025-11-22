6514d627ad3e98c701e8898f63a5bab612b2a36b
dprashanth-kumar <dprashanthtinku@gmail.com>
2025-11-22 17:27:20 +0530

    Update second time added default values to variable.tf

--- variable.tf
+++ variable.tf
@@ -6,19 +6,23 @@ variable "project_name" {
 variable "vpc_cidr" {
   description = "VPC_CIDR"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnets" {
   description = "Public subnet CIDRs"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets" {
   description = "Private subnet CIDRs"
   type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "availability_zones" {
   description = "List of AZs"
   type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
 }
