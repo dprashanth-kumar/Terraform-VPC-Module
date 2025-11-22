ac255da03d27a01c6f3af029f957a1e6ad7b6747
dprashanth-kumar <dprashanthtinku@gmail.com>
2025-11-22 17:05:25 +0530

    Created first main.tf
    Added first main.tf file into repo

---
+++ main.tf
@@ -0,0 +1,64 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "${var.project_name}-public-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id     = aws_vpc.this.id
+  cidr_block = var.private_subnets[count.index]
+
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "${var.project_name}-private-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  gateway_id             = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
